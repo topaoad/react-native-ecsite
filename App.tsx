4f118a78588d01c131b582b3f42b729f74b4d8cd
toproad <tattun0817@gmail.com>
2023-10-04 20:26:55 +0900

    初回

---
+++ App.tsx
@@ -0,0 +1,77 @@
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { Firestore, collection, getDocs, getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { useEffect, useState } from "react";
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_FIREBASE_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+type Shop = {
+  id: string;
+  name: string;
+  place: string;
+};
+
+export default function App() {
+  const [shops, setShops] = useState<Shop[]>([]);
+  useEffect(() => {
+    const querySnapshot = async () => {
+      const snapshot = await getDocs(collection(db, "shops"));
+      const shops = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        name: doc.data().name,
+        place: doc.data().place,
+      }));
+      setShops(shops);
+    };
+    querySnapshot();
+  }, []);
+  console.log(shops);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.flatListContainer}>
+        <FlatList
+          data={shops}
+          renderItem={({ item }) => (
+            <View style={styles.flatListContainer}>
+              <Text>{item.name}</Text>
+              <Text>{item.place}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={{ alignItems: "center" }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  flatListContainer: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,32 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+          safe: true,
+        },
+      ],
+    ],
   };
 };
+
+// module.exports = {
+//   presets: ['module:metro-react-native-babel-preset'],
+//   plugins: [
+//     [
+//       'module-resolver',
+//       {
+//         root: ['./'],
+//         alias: {
+//           '@env': './.env',
+//         },
+//       },
+//     ],
+//   ],
+// };
